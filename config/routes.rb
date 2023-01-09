31e621d94b15ce7226179cbdccffa00f477b8019
Agata Mostowik <agatamostowik@gmail.com>
2023-01-09 21:23:46 +0100

    Initial Commit

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  resources :articles
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  root "articles#index"
+end
+
+Rails.application.routes.draw do
+  # root "articles#index"
+
+  resources :articles do
+    resources :comments
+  end
+end
